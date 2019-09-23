2b8dab224cf507d6e954605c0aefc022db16dd6e
andrewwaters00 <andrew.waters00@googlemail.com>
2019-09-23 11:18:42 +0100

    Packer terraform main.tf

---
+++ main.tf
@@ -0,0 +1,26 @@
+variable "access_key" {}
+variable "secret_key" {}
+
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "eu-west-2"
+}
+
+data "aws_ami" "dev_ami" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["packer-example*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["378228206711"]
+}
+
+
